3a714ef91128458e6467dbc41248db827521d833
tien trinh <nguusaongu@gmail.com>
2020-10-10 19:12:53 +0700

    á

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { UserComponent } from './user/user.component';
+import { MoviesComponent } from './movies/movies.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      { path: 'users', component: UserComponent, data: { title: 'Users' } },
+      { path: 'movies', component: MoviesComponent, data: { title: 'Movies' } },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, AddMovieComponent, UserComponent, MoviesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes), 
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
